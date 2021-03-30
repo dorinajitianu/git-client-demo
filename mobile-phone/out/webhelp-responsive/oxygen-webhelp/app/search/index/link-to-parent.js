94813bc281dbac897345f6f24c1abf0a4cd4cc3a
AlexJitianu <jitianualex83@gmail.com>
2021-03-30 17:28:22 +0300

    Made it better.

---
+++ mobile-phone/out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {17:16,5:19,3:16,16:-1,23:-1,7:0,9:0,2:16,0:23,8:16,22:-1,1:15,6:19,20:22,19:-1,13:19,4:19,12:16,21:22,10:15,15:23,18:16,11:0,14:15};
+});
